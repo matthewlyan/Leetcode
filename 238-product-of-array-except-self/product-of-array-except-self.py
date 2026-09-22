c96689a8fb00b5278bed2206c68d680fb7771b57
matthewlyan <54650027+matthewlyan@users.noreply.github.com>
2026-09-21 22:41:37 -0400

    Time: 34 ms (26.11%) | Memory: 27.9 MB (7.63%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,37 +1,20 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # answer[i] = product of everything to the left of i * product of everything to the right of i
-        #prefix[i] - means multiply everything before index i, prefix[i-1] * nums[i-1]
-        #suffix[i] - means multiply everything to the right of index i, suffix[i+1] * nums[i+1]
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        # this one we need one for the prefix
+        #one for the suffix and then multiply them all together to get the number
 
-        #prefix[i-1] - everything before the previous guy
-        #nums[i-1] - the previous guy
-
-        # suffix[i+1] - everything after the next i
-        # nums[i+1] - the next i
-
-        prefix = [1] * len(nums)
-        suffix = [1] * len(nums)
+        left = [1] * len(nums)
+        right = [1] * len(nums)
 
         for i in range(1,len(nums)):
-            prefix[i] = prefix[i-1] * nums[i-1]
-
-        for i in range(len(nums) - 2, -1, -1):
-            suffix[i] = suffix[i+1] * nums[i+1]
+            left[i] = nums[i-1] * left[i-1]
         
-        # suffix[2] = suffix[3] * nums[3] = 1 * 4 = 4
-        # suffix = [1,1,4,1]
-
-        #suffix[1] = suffix[2] * nums[2] = 4 * 3
+        for i in range(len(nums) - 2,-1,-1):
+            right[i] = nums[i+1] * right[i+1]
+        
         res = [1] * len(nums)
+
         for i in range(len(nums)):
-            res[i] = prefix[i] * suffix[i]
-        
+            res[i] = left[i] * right[i]
+
         return res
-
-
-
-
-
-
-
